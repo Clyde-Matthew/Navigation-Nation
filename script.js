ca8e4b8776b6987907addab6d9c91e9797d91b8f
Clyde-Matthew <clyde.matthew1@gmail.com>
2022-03-25 22:10:57 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const menuBars = document.getElementById("menu-bars");
+const overlay = document.getElementById("overlay");
+const nav1 = document.getElementById("nav-1");
+const nav2 = document.getElementById("nav-2");
+const nav3 = document.getElementById("nav-3");
+const nav4 = document.getElementById("nav-4");
+const nav5 = document.getElementById("nav-5");
+const navItems = [nav1, nav2, nav3, nav4, nav5];
+
+// control navigation Animation
+function navAnimation(direction1, direction2) {
+  navItems.forEach((nav, i) => {
+    nav.classList.replace(
+      `slide-${direction1}-${i + 1}`,
+      `slide-${direction2}-${i + 1}`
+    );
+  });
+}
+
+function toggleNav() {
+  // Toggle menu bars open/closed
+  menuBars.classList.toggle("change");
+  // Toggle: menu active
+  overlay.classList.toggle("overlay-active");
+  if (overlay.classList.contains("overlay-active")) {
+    // animate overlay in
+    overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
+    // animate nav items it
+    navAnimation("out", "in");
+  } else {
+    // animate overlay out
+    overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
+    // animate nav items out
+    navAnimation("in", "out");
+  }
+}
+// Event Listeners
+
+menuBars.addEventListener("click", toggleNav);
+navItems.forEach((nav) => {
+  nav.addEventListener("click", toggleNav);
+});
